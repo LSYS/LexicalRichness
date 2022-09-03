85f89162a7738a2c6e2a0be1ab3da997f19fbb43
LSYS <lucas@lucasshen.com>
2022-09-03 14:45:46 +0800

    add tests for vocd (#42)
    - add tests for ttr_nd (McKee Mavern Richards 2000) and for vocd() method (#42)

--- tests/test_lexicalrichness.py
+++ tests/test_lexicalrichness.py
@@ -10,43 +10,61 @@ from lexicalrichness.lexicalrichness import preprocess
 from lexicalrichness.lexicalrichness import tokenize
 from lexicalrichness.lexicalrichness import list_sliding_window
 from lexicalrichness.lexicalrichness import segment_generator
+from lexicalrichness.lexicalrichness import ttr_nd
 import pytest
+import numpy as np
 
 
 class TestLexicalrichness(unittest.TestCase):
     """Tests for `lexicalrichness` package."""
 
     def setUp(self):
-        print('setting up')
+        print("setting up")
         self.s1 = "TEST text with some text numbers 42, hyphen-here, and text punctuations."
         self.s2 = "TEST text with some text numbers 42, hyphen–here, and text punctuations."
         self.s3 = "TEST text with some text numbers 42, hyphen—here, and text punctuations."
-        self.emptystring = ''
+        self.emptystring = ""
+        self.longtext = """Measure of textual lexical diversity, computed as the mean length of sequential words in
+                a text that maintains a minimum threshold TTR score.
+
+                Iterates over words until TTR scores falls below a threshold, then increase factor
+                counter by 1 and start over. McCarthy and Jarvis (2010, pg. 385) recommends a factor
+                threshold in the range of [0.660, 0.750].
+                (McCarthy 2005, McCarthy and Jarvis 2010)"""
 
         self.obj1 = LexicalRichness(self.s1)
         self.obj2 = LexicalRichness(self.s2)
         self.obj3 = LexicalRichness(self.s3)
         self.emptyobj = LexicalRichness(self.emptystring)
+        self.longtext = LexicalRichness(self.longtext)
 
     def tearDown(self):
-        print('tearing down\n')
+        print("tearing down\n")
 
     def test_preprocess(self):
-        print('testing preprocess')
+        print("testing preprocess")
 
-        self.assertEqual(preprocess(self.s1), 'test text with some text numbers , hyphenhere, and text punctuations.')
-        self.assertEqual(preprocess(self.s2), 'test text with some text numbers , hyphenhere, and text punctuations.')
-        self.assertEqual(preprocess(self.s3), 'test text with some text numbers , hyphenhere, and text punctuations.')
-        self.assertEqual(preprocess(self.emptystring), '')
+        self.assertEqual(
+            preprocess(self.s1),
+            "test text with some text numbers , hyphenhere, and text punctuations.",
+        )
+        self.assertEqual(
+            preprocess(self.s2),
+            "test text with some text numbers , hyphenhere, and text punctuations.",
+        )
+        self.assertEqual(
+            preprocess(self.s3),
+            "test text with some text numbers , hyphenhere, and text punctuations.",
+        )
+        self.assertEqual(preprocess(self.emptystring), "")
 
     def test_tokenize(self):
-        print('testing tokenize')
+        print("testing tokenize")
 
         self.assertIs(type(tokenize(self.s1)), list)
         self.assertIs(type(tokenize(self.s2)), list)
         self.assertIs(type(tokenize(self.s3)), list)
         self.assertIs(type(tokenize(self.emptystring)), list)
-
 
     def test_tokenize_str_error(self):
         """Ensures error is raised if tokenizer is set to None and input is a string."""
@@ -54,68 +72,84 @@ class TestLexicalrichness(unittest.TestCase):
             LexicalRichness(self.s1, tokenizer=None)
         assert str(err.value) == "If tokenizer is None, then input should be a list of words."
 
-
     def test_list_sliding_window(self):
-        print('testing list_sliding_window')
+        print("testing list_sliding_window")
 
-        test_list = ['a', 'b', 'c', 'd']
+        test_list = ["a", "b", "c", "d"]
 
-        self.assertEqual(list(list_sliding_window(test_list, 1)), [('a',), ('b',), ('c',), ('d',)])
-        self.assertEqual(list(list_sliding_window(test_list, 2)), [('a', 'b'), ('b', 'c'), ('c', 'd')])
-        self.assertEqual(list(list_sliding_window(test_list, 3)), [('a', 'b', 'c'), ('b', 'c', 'd')])
-        self.assertEqual(list(list_sliding_window(test_list, 4)), [('a', 'b', 'c', 'd')])
+        self.assertEqual(
+            list(list_sliding_window(test_list, 1)), [("a",), ("b",), ("c",), ("d",)]
+        )
+        self.assertEqual(
+            list(list_sliding_window(test_list, 2)), [("a", "b"), ("b", "c"), ("c", "d")]
+        )
+        self.assertEqual(
+            list(list_sliding_window(test_list, 3)), [("a", "b", "c"), ("b", "c", "d")]
+        )
+        self.assertEqual(list(list_sliding_window(test_list, 4)), [("a", "b", "c", "d")])
 
     def test_segment_generator(self):
-        print('testing segment_generator')
+        print("testing segment_generator")
 
         test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-        self.assertEqual(list(segment_generator(test_list, 3)), [[1,2,3], [4,5,6], [7,8,9], [10]])
-        self.assertEqual(list(segment_generator(test_list, 5)), [[1,2,3,4,5], [6,7,8,9,10]])
-        self.assertEqual(list(segment_generator(test_list, 1)), [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
-        self.assertEqual(list(segment_generator(test_list, 10)), [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
-        self.assertEqual(list(segment_generator(test_list, 11)), [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
+        self.assertEqual(
+            list(segment_generator(test_list, 3)), [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]]
+        )
+        self.assertEqual(
+            list(segment_generator(test_list, 5)), [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+        )
+        self.assertEqual(
+            list(segment_generator(test_list, 1)),
+            [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]],
+        )
+        self.assertEqual(
+            list(segment_generator(test_list, 10)), [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
+        )
+        self.assertEqual(
+            list(segment_generator(test_list, 11)), [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
+        )
 
     def test_words(self):
-        print('testing words')
+        print("testing words")
         self.assertEqual(self.obj1.words, 10)
         self.assertEqual(self.emptyobj.words, 0)
 
     def test_terms(self):
-        print('testing terms')
+        print("testing terms")
         self.assertEqual(self.obj1.terms, 8)
         self.assertEqual(self.emptyobj.terms, 0)
 
     def test_ttr(self):
-        print('testing ttr')
+        print("testing ttr")
         self.assertEqual(self.obj1.ttr, 0.8)
 
     def test_rttr(self):
-        print('testing rttr')
+        print("testing rttr")
         self.assertEqual(self.obj1.rttr, 2.5298221281347035)
 
     def test_cttr(self):
-        print('testing cttr')
+        print("testing cttr")
         self.assertEqual(self.obj1.cttr, 1.7888543819998317)
 
     def test_Herdan(self):
-        print('testing Herdan')
+        print("testing Herdan")
         self.assertEqual(self.obj1.Herdan, 0.9030899869919434)
 
     def test_Summer(self):
-        print('testing Summer')
+        print("testing Summer")
         self.assertEqual(self.obj1.Summer, 0.8777828395738175)
 
     def test_Dugast(self):
-        print('testing Dugast')
+        print("testing Dugast")
         self.assertEqual(self.obj1.Dugast, 23.760032854423635)
 
     def test_Maas(self):
-        print('testing Maas')
+        print("testing Maas")
         self.assertEqual(self.obj1.Maas, 0.04208748389057132)
 
     def test_msttr(self):
-        print('testing msttr')
+        print("testing msttr")
 
         self.assertEqual(self.obj1.msttr(segment_window=5, discard=True), 0.8)
         self.assertEqual(self.obj1.msttr(segment_window=5, discard=False), 0.9)
@@ -128,7 +162,7 @@ class TestLexicalrichness(unittest.TestCase):
             self.obj1.msttr(segment_window=1.5)
 
     def test_mattr(self):
-        print('testing mattr')
+        print("testing mattr")
 
         self.assertEqual(self.obj1.mattr(window_size=5), 0.9)
         self.assertEqual(self.obj1.mattr(window_size=1), 1)
@@ -142,15 +176,15 @@ class TestLexicalrichness(unittest.TestCase):
             self.obj1.mattr(window_size=1.5)
 
     def test_mtld(self):
-        print('testing mtld')
+        print("testing mtld")
 
         self.assertEqual(self.obj1.mtld(threshold=0.72), 14.000000000000004)
 
-        all_unqiue = LexicalRichness('only unique terms in this little string')
+        all_unqiue = LexicalRichness("only unique terms in this little string")
         self.assertEqual(all_unqiue.mtld(threshold=0.72), all_unqiue.words)
 
     def test_hdd(self):
-        print('testing hdd')
+        print("testing hdd")
 
         self.assertEqual(self.obj1.hdd(draws=5), 0.8833333333333332)
 
@@ -161,6 +195,21 @@ class TestLexicalrichness(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.obj1.hdd(draws=1.5)
 
+    def test_ttr_nd(self):
+        self.assertEqual(ttr_nd(N=100, D=2), 0.1809975124224178)
 
-if __name__ == '__main__':
+    def test_vocd(self):
+        print("testing voc-D")
+        self.assertIs(type(self.longtext.vocd()), np.float64)
+
+        # Test that random seed works (for reproducibility)
+        first_seed42 = self.longtext.vocd()
+        second_seed42 = self.longtext.vocd()
+        third_seed0 = self.longtext.vocd(seed=0)
+        assert first_seed42 == second_seed42
+        assert first_seed42 != third_seed0
+        assert second_seed42 != third_seed0
+
+
+if __name__ == "__main__":
     unittest.main()
