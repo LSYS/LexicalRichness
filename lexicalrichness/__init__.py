766e7b13354f8a6f4a85cf4e6e6113a9b6ccdb67
Lucas Shen Y. S <lucas@lucasshen.com>
2022-12-25 22:50:26 +0800

    Bump to correct version

--- lexicalrichness/__init__.py
+++ lexicalrichness/__init__.py
@@ -3,9 +3,8 @@
 """Top-level package for LexicalRichness."""
 
 __title__ = 'lexicalrichness'
-__author__ = """Shen Yan Shun (Lucas)"""
 __author__ = """Lucas Shen YS"""
-__email__ = 'shen1ys@gmail.com'
-__version__ = '0.1.3'
+__email__ = 'lucas@lucasshen.com'
+__version__ = '0.3.1'
 
 from .lexicalrichness import *
